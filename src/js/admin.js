f0e391155a8e3be53e3e294366ad27321879153d
Daeseong Yu <yu5501@nate.com>
2018-07-19 14:28:48 +0900

    first_divide~fourth_divide 총 네개의 파일을 setting_option으로 통합 admin_header, admin_footer를 개별 파일 안쪽에 선언

--- src/js/admin.js
+++ src/js/admin.js
@@ -4,61 +4,15 @@ function numberWithCommas(x) {
 }
 
 jQuery(function($) {
-  $(document).ready(function(){
-    $("#header").load("./admin_header.html");
-    $("#footer").load("./admin_footer.html");
-  });
-
-  // 예약 종류, 투어 선택 클릭 시 펼쳐지는 토글 이벤트
+  // 우측 메뉴바 이벤트
   $(function () {
-    $(".td_container").click(function(){
-      if ($(this).siblings(".hidden_toggle_box").css("display") === "none"){
-        $(this).siblings(".hidden_toggle_box").css("display", "block");
-      }
-      else {
-        $(this).siblings(".hidden_toggle_box").css("display", "none");
-      }
-    });
-  });
-
-  // 분류 선택 버튼
-  $(function() {
-    $(".each_select_box > button").click(function(){
-
-      var documentWidth = $(document).width();
-      var documentHeight = $(document).height();
-
-      $('#escape_box').css("display", "block");
-      $('#escape_box').css({'width':documentWidth,'height':documentHeight});
-
-      var m = $('.each_select_box > button').index(this);
-
-      if($(this).siblings(".toggle_box").css("display") === "none"){
-        $(this).siblings(".toggle_box").css("display", "block");
+    $(".head_menu_icon_area").click(function(){
+      if($("#menu_box").css("display") === "none") {
+        var targetHeight = $(window).height() - 60;
+        $("#menu_box").css({"display": "block", 'height': targetHeight});
       }else{
-        $(this).siblings(".toggle_box").css("display", "none");
+        $("#menu_box").css("display", "none");
       }
-    });
-  });
-
-  // 펼쳐진 토글 창의 목록 중의 컴포넌트 클릭 시 해당 값이 버튼으로 입력 됨
-  $(function () {
-    $(".toggle_content").click(function () {
-      var selected_content = $(this).text();
-
-      $(this).parent(".toggle_box").siblings("button").children("span").text(selected_content);
-      $(this).parent(".toggle_box").css("display", "none");
-      $("#escape_box").css("display", "none");
-    });
-  });
-
-  //  펼쳐진 토글 창의 컴포넌트가 아닌 외부를 클릭하여도 벗어날 수 있게함
-  $(function () {
-    $("#escape_box").click(function () {
-      $("#escape_box").css("display", "none");
-      $(".toggle_box").each(function(index){
-        $(".toggle_box:eq("+index+")").css("display", "none");
-      });
     });
   });
 
@@ -78,26 +32,29 @@ jQuery(function($) {
 
   // 분류 추가를 위한 매소드
   $(function (){
-    $(".category_control_box > button").click(function() {
-      var sel_1 = $("#first_divide_select").children("span").text();
-      var sel_2 = $("#second_divide_select").children("span").text();
-      var sel_3 = $("#third_divide_select").children("span").text();
-      var sel_4 = $("#insert_category_input").val();
+    $(".rectangle_select_box > button").click(function() {
 
-      if(sel_1 == "대분류 선택"){
+      var sel_1 = $("#big_divide_select").val();
+      var sel_2 = $("#middle_divide_select").val();
+      var sel_3 = $("#small_divide_select").val();
+
+      var adult_price = $("#insert_adult_price").val().replace(/[^(0-9)\.]/g, "");
+      adult_price = parseFloat(adult_price).toFixed(2);
+      var kid_price = $("#insert_kid_price").val().replace(/[^(0-9)\.]/g, "");
+      kid_price = parseFloat(kid_price).toFixed(2);;
+      var baby_price = $("#insert_baby_price").val().replace(/[^(0-9)\.]/g, "");
+      baby_price = parseFloat(baby_price).toFixed(2);
+
+      if(sel_1 == ""){
         alert("대분류를 선택하세요.");
         return;
       }
-      else if(sel_2 == "중분류 선택"){
+      else if(sel_2 == ""){
         alert("중분류를 선택하세요.");
         return;
       }
-      else if(sel_4 == "소분류 선택"){
+      else if(sel_3 == ""){
         alert("소분류를 선택하세요.");
-        return;
-      }
-      else if(sel_4 == ""){
-        alert("분류명을 입력하세요.");
         return;
       }
       else if(adult_price == "NaN"){
@@ -105,52 +62,41 @@ jQuery(function($) {
         return;
       }
 
+      adult_price = "$" + numberWithCommas(adult_price);
+      if(kid_price == "NaN"){
+        kid_price = "선택 불가";
+      }else{
+        kid_price = "$" + numberWithCommas(kid_price);
+      }
+      if(baby_price == "NaN"){
+        baby_price = "선택 불가";
+      }else{
+        baby_price = "$" + numberWithCommas(baby_price);
+      }
+
+
       var add_div = "<div class = \"tr_container\"><div class = \"td_container\">";
 
-      if(!($("#first_divide_select").length)){
-        add_div += "<div class = \"td_cell\">"+sel_4+"</div>";
-      }else if(!($("#second_divide_select").length)){
-        add_div += "<div class = \"td_cell\">"+sel_1+"</div>";
-        add_div += "<div class = \"td_cell\">"+sel_4+"</div>";
-      }else if(!($("#third_divide_select").length)){
-        add_div += "<div class = \"td_cell\">"+sel_1+"</div>";
-        add_div += "<div class = \"td_cell\">"+sel_2+"</div>";
-        add_div += "<div class = \"td_cell\">"+sel_4+"</div>";
-      }else{
-
-        var adult_price = $("#insert_adult_price").val().replace(/[^(0-9)\.]/g, "");
-        adult_price = parseFloat(adult_price).toFixed(2);
-        var kid_price = $("#insert_kid_price").val().replace(/[^(0-9)\.]/g, "");
-        kid_price = parseFloat(kid_price).toFixed(2);;
-        var baby_price = $("#insert_baby_price").val().replace(/[^(0-9)\.]/g, "");
-        baby_price = parseFloat(baby_price).toFixed(2);
-
-        adult_price = "$" + numberWithCommas(adult_price);
-        if(kid_price == "NaN"){
-          kid_price = "선택 불가";
-        }else{
-          kid_price = "$" + numberWithCommas(kid_price);
-        }
-        if(baby_price == "NaN"){
-          baby_price = "선택 불가";
-        }else{
-          baby_price = "$" + numberWithCommas(baby_price);
-        }
-
-        add_div += "<div class = \"td_cell\">"+sel_1+"</div>";
-        add_div += "<div class = \"td_cell\">"+sel_2+"</div>";
-        add_div += "<div class = \"td_cell\">"+sel_3+"</div>";
-        add_div += "<div class = \"td_cell\">"+sel_4+"</div>";
-        add_div += "<div class = \"td_cell_small\">"+adult_price+"</div>";
-        add_div += "<div class = \"td_cell_small\">"+kid_price+"</div>";
-        add_div += "<div class = \"td_cell_small\">"+baby_price+"</div>";
-      }
+      add_div += "<div class = \"td_cell\">"+sel_1+"</div>";
+      add_div += "<div class = \"td_cell\">"+sel_2+"</div>";
+      add_div += "<div class = \"td_cell\">"+sel_3+"</div>";
+      add_div += "<div class = \"td_cell_small\">"+adult_price+"</div>";
+      add_div += "<div class = \"td_cell_small\">"+kid_price+"</div>";
+      add_div += "<div class = \"td_cell_small\">"+baby_price+"</div>";
 
       add_div += "</div><div class = \"del_btn_container\"><div class = \"del_text\">삭제</div></div></div>";
 
       $(".table_container").append(add_div);
+
+      $("#big_divide_select").val("");
+      $("#middle_divide_select").val("");
+      $("#small_divide_select").val("");
+      $("#insert_adult_price").val("");
+      $("#insert_kid_price").val("");
+      $("#insert_baby_price").val("");
     });
   });
+
 
   // 삭제 버튼 클릭 시 해당 라인 삭제
   $(document).on("click",".del_btn_container",function(){
